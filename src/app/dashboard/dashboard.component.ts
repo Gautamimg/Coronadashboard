0cd30e1a3b83fc5f876c40d5b96fc3e3fba5d9b8
Gautamimg <gautam12.img@gmail.com>
2023-06-01 09:39:49 +0530

    fist changes

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {ServiceService} from '../service.service';
+import { ChartComponent } from "ng-apexcharts";
+
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  wordwide: any;
+  countrywise_data: any;
+  chartOptions:any;
+
+  constructor(private serve:ServiceService) { 
+    
+  }
+
+  ngOnInit(): void {
+    this.serve.Coronadata_api().subscribe((res:any)=>{
+      console.log("data",res);
+      this.wordwide=res.world_total;
+      this.countrywise_data=res.countries_stat
+   console.log("country",this.countrywise_data);
+      console.log(this.wordwide)
+    });
+
+    this.chartOptions = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        type: "donut"
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };  }
+
+}
---
+++ src/app/service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  constructor(private http:HttpClient) {}
+  httpOptions = { headers: new HttpHeaders().set('X-RapidAPI-Key','b84fe28636msh599fd7625712ab4p16e54djsn3ff298fcb21b').set('X-RapidAPI-Host','corona-virus-world-and-india-data.p.rapidapi.com')};
+  Coronadata_api(){
+   return this.http.get("https://corona-virus-world-and-india-data.p.rapidapi.com/api",this.httpOptions);
+  }
+}
